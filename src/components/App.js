ffa4a101048f25142dcb0fe1a690042c2bec16fb
mrh9ud <mrh9ud@virginia.edu>
2020-01-09 18:04:50 -0500

    most functionality working

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,30 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (event) => {
+    this.setState({
+      filters: {
+        type: event.target.value
+      }
+    })
+  }
+
+  onFindPetsClick = () => {
+    if (this.state.filters.type !== 'all') {
+      fetch(`/api/pets?type=${this.state.filters.type}`)
+      .then(res => res.json())
+      .then(pets => this.setState({ pets: pets }))
+    } else {
+      fetch('/api/pets')
+      .then(res => res.json())
+      .then(pets => this.setState({ pets: pets }))
+    }
+  }
+
+  onAdoptPet = (id) => {
+    console.log('takes an id for a pet from state.pet and set isAdopted to true')
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +48,16 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters 
+                onChangeType={this.onChangeType} 
+                onFindPetsClick={this.onFindPetsClick}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser 
+                pets={this.state.pets}
+                onAdoptPet={this.onAdoptPet}  
+              />
             </div>
           </div>
         </div>
